af55dca5b94d01f2a569235d661eca58d081c33b
Monishwar Reddy Vardireddy <149665838+monishwar-reddy@users.noreply.github.com>
2025-11-20 14:25:12 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseUrl = 'https://nazgwjwnruxfmbbvpeft.supabase.co'
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hemd3anducnV4Zm1iYnZwZWZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODk5OTEsImV4cCI6MjA3ODk2NTk5MX0.-fMo2lushMvQUTlyppV7IN3Gx3lV8_YOdqmhoXiHrwE'
+export const supabaseUrl = 'Sample'
+export const supabaseAnonKey = 'Sample'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
